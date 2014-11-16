e9fba03870ebac8a1297de03cf26d7bc6a0bb373
randName <kwokshungit@gmail.com>
2014-11-16 07:32:21 +0000

    minor bugs

--- main.pl
+++ main.pl
@@ -15,6 +15,8 @@ my @MORAS = qw(ya yu yo wa wo nn);
 foreach my $c ('',qw(k s t n h m r)){ foreach (qw(a i u e o)) { push(@MORAS,$c.$_); } }
 
 my $content;
+my $p = XML::RSS::Parser->new;
+my $feed; my @works = ();
 
 sub write_to_file
 {
@@ -33,20 +35,20 @@ sub read_from_file
 
 sub get_count
 {
-  my ( $type, $id, $domain ) = @_ ;
-  my $ct; my @count = (0,0);
-  
-  if ( $domain & 1 )
-  {
-    $ct = get( "$DMM/$DOM[0]/list/=/article=$type/id=$id" );
-    ($count[0]) = $ct =~ m/(\d+).*タイトル中/ig ;
-  }
+	my ( $type, $id, $domain ) = @_ ;
+	my $ct; my @count = (0,0);
+	  
+	if ( $domain & 1 )
+	{
+		$ct = get( "$DMM/$DOM[0]/list/=/article=$type/id=$id" );
+		($count[0]) = $ct =~ m/(\d+).*タイトル中/ig ;
+	}
 
-  if ( $domain & 2 )
-  {
-    $ct = get( "$DMM/$DOM[1]/list=/article=$type/id=$id" );
-    ($count[1]) = $ct =~ m/(\d+).*?タイトル中/ ;
-  }
+	if ( $domain & 2 )
+	{
+		$ct = get( "$DMM/$DOM[1]/list/=/article=$type/id=$id" );
+		($count[1]) = $ct =~ m/(\d+).*?タイトル中/ ;
+	}
 
 	if ( $domain == 3 ){ return @count; } else { return $count[$domain-1]; }
 }
@@ -164,4 +166,56 @@ sub get_actresses
 	return \%acts;
 }
 
+sub get_pages
+{
+	my ( $q, $i_start, $i_end, $step ) = @_ ;
 
+	my $p_end = int( $i_end/$step );
+	if ( $i_end%$step > 0 ){ $p_end += 1; }
+
+	print "$i_start\t$i_end\t$p_end\t$step\n";
+
+	for ( my $i = int( $i_start/$step )+1; $i <= $p_end; $i++ )
+	{
+		$feed = $p->parse_uri( "$q/limit=$step/page=$i/" );
+
+		if ( not defined $feed )
+		{
+			if ( $step != 1 )
+			{
+				get_pages( $q, ($i-1)*$step+1, $i*$step, $step/5 );
+			}
+			else { print "error at $i\n"; }
+		}
+		else
+		{
+			foreach ( $feed->query('//item') ){ push( @works, get_items($_) ); }
+		}
+	}
+}
+
+sub get_items
+{
+	my %v; my $content = $_->query('content:encoded')->text_content;
+
+	$v{'title'} = $_->query('title')->text_content;
+	$v{'url'} = $_->query('link')->text_content;
+	$v{'description'} = $_->query('description')->text_content;
+	$v{'date'} = $_->query('dc:date')->text_content;
+
+	($v{'cid'}) = $v{'url'} =~ m/cid=([^\/]+)/ ;
+	($v{'runtime'}) = $content =~ m/>([0-9]+)/ ;
+	($v{'label'}) = $content =~ m/article=label\/id=([0-9]+)/g ;
+	($v{'series'}) = $content =~ m/article=series\/id=([0-9]+)/g ;
+	$v{'actress'} = [ $content =~ m/article=actress\/id=([0-9]+)/g ];
+
+	$v{'tags'} = [ $content =~ m/article=keyword\/id=([0-9]+)/g ];
+
+	#print "$_ $v{$_}\n" for keys %v; print "\n";
+	#print "$v{'cid'}\t$v{'title'}\n";
+	
+	return \%v;
+}
+
+#get_pages( "$DMM/$DOM[0]/list/$RSS[0]/$PARAMS/id=$id", 1, $count, 125 );
+#get_pages( "$DMM/$DOM[1]/list/$RSS[1]/$PARAMS/id=$id", 1, $count, 125 );
