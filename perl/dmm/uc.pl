4d08aa3061ab45c86023fc1997759503753dfc2c
Shun Git <kwokshungit@gmail.com>
2015-11-08 08:52:22 +0000

    remove perl folder

--- perl/dmm/uc.pl
+++
@@ -1,8 +0,0 @@
-open ( MAKER, ">maker_dvd_counts" ) || die "Can't open: $!\n";
-
-{
-		$count = get( "$DMM/list/=/article=maker/id=$id" );
-		($count) = $count =~ m/(\d+).*?タイトル中/ ;
-		print MAKER "$id\t$count\n";
-}
-close( MAKER );
