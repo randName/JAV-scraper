ac643feab52d39d1cb3632dd9ab55aefa5150631
Shun Git <kwokshungit@gmail.com>
2016-01-09 07:39:11 +0000

    genericize proxy

--- image_proxy.py
+++ image_proxy.py
@@ -2,31 +2,10 @@ import http.server, requests
 from urllib.parse import urlparse, parse_qs
 
 def get_img( path ):
-    DOM = ( 'digital/video', 'mono/movie/adult' )
 
-    p = urlparse(path)
+    r = requests.get( "http://pics.dmm.co.jp/" + path, allow_redirects=False )
 
-    l = p.path
-
-    if l == 'favicon.ico': return None
-
-    s = 'pt'
-    d = 0
-
-    if '=' in p.query:
-        qs = parse_qs(p.query)
-        if 'd' in qs: d = int(qs['d'][0])
-        if 's' in qs: s = qs['s'][0]
-    elif p.query:
-        s = p.query
-
-    q = "http://pics.dmm.co.jp/%s/%s/%s%s.jpg" % ( DOM[d], l, l, s )
-    r = requests.get( q, allow_redirects=False )
-
-    if r.status_code == 200:
-        return r.content
-    else:
-        return None
+    return r.content if r.status_code == 200 else None
 
 def empty(s):
     s.send_header("Content-type", "text/html")
@@ -34,7 +13,7 @@ def empty(s):
     page = "<html><head><title>t</title></head><body>%s</body></html>" % s.path
     s.wfile.write(page.encode())
 
-class ImgHandler( http.server.SimpleHTTPRequestHandler ):
+class DMMHandler( http.server.SimpleHTTPRequestHandler ):
 
     def do_HEAD(s):
         s.send_response(200)
@@ -43,14 +22,24 @@ class ImgHandler( http.server.SimpleHTTPRequestHandler ):
 
     def do_GET(s):
         s.send_response(200)
-        s.send_header("Content-type", "image/jpeg")
-        s.end_headers()
 
-        img = get_img(s.path)
-        s.wfile.write( img if img else b'\xff\xd8\xff\xd9' )
+        if '.jpg' in s.path:
+            s.send_header("Content-type", "image/jpeg")
+            s.end_headers()
+
+            img = get_img( s.path )
+            s.wfile.write( img if img else b'\xff\xd8\xff\xd9' )
+
+        elif '.mp4' in s.path:
+            s.send_header("Content-type", "video/mp4")
+            s.end_headers()
+
+        else:
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
 
 def run(port=3000):
-    httpd = http.server.HTTPServer( ( '', port ), ImgHandler )
+    httpd = http.server.HTTPServer( ( '', port ), DMMHandler )
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
